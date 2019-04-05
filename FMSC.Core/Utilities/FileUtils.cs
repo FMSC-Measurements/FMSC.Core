437e67bf3c00fc943e570276a16981e6a0a22f66
Bryan Miller <bryancmiller@fs.fed.us>
2019-04-05 13:28:20 -0600

    Added FileUtils

---
+++ FMSC.Core/Utilities/FileUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FMSC.Core.Utilities
+{
+    public static class FileUtils
+    {
+        public static bool IsFileLocked(string filename, FileAccess file_access = FileAccess.Read)
+        {
+            try
+            {
+                new FileStream(filename, FileMode.Open, file_access).Close();
+                return false;
+            }
+            catch (IOException)
+            {
+                return true;
+            }
+        }
+    }
+}
