f6c26339bbdddaaee2b59b3a5002b90980f0a7da
bmillerfs <bryan.c.miller@usda.gov>
2023-09-21 21:04:13 -0600

    Added PriorityChceckedListItem

---
+++ FMSC.Core.Windows/ComponentModel/PriorityCheckedListItem.cs
@@ -0,0 +1,22 @@
+﻿using FMSC.Core.ComponentModel;
+using System;
+
+namespace FMSC.Core.Windows.ComponentModel
+{
+    public class PriorityCheckedListItem<T> : CheckedListItem<T>
+    {
+        private bool _IsPriority;
+        public bool IsPriority
+        {
+            get { return _IsPriority; }
+            set { SetField(ref _IsPriority, value); }
+        }
+
+        public PriorityCheckedListItem() { }
+
+        public PriorityCheckedListItem(T item, bool isChecked = false, bool isPriority = false) : base(item, isChecked)
+        {
+            IsPriority = isPriority;
+        }
+    }
+}
