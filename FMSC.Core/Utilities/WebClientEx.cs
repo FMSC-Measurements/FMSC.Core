aa2974ba2ca9c28247ac953682106c656ca2711f
bmillerfs <bryan.c.miller@usda.gov>
2024-04-02 18:24:31 -0600

    Added WebClientEx

---
+++ FMSC.Core/Utilities/WebClientEx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+
+namespace FMSC.Core.Utilities
+{
+    public class WebClientEx : WebClient
+    {
+        private readonly int _Timeout;
+
+        public WebClientEx(int seconds = 30)
+        {
+            _Timeout = seconds;
+        }
+
+        protected override WebRequest GetWebRequest(Uri uri)
+        {
+            WebRequest w = base.GetWebRequest(uri);
+            w.Timeout = _Timeout * 1000;
+            return w;
+        }
+    }
+}
